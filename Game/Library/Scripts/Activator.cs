a3d7e5eac96b267ce4b0b65949e1938f9b4ef3bb
Wilhelman <guillermogarsub@gmail.com>
2019-03-03 19:21:54 +0100

    updatee

---
+++ Game/Library/Scripts/Activator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using JellyBitEngine;
+using JellyBitEngine.UI;
+
+public class Activator : JellyScript
+{
+    bool imageActive = true;
+    public LayerMask LayerMask = new LayerMask();
+    public enum TEST
+    {
+        CALABASA,
+        ARKAXOFA,
+        SIDA,
+        MATENME
+    }
+
+    public TEST testEnum;
+
+    //Use this method for initialization
+    public override void Awake()
+    {
+
+    }
+
+    //Called every frame
+    public override void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.KEY_1))
+        {
+            testEnum.GetType();
+            System.Enum.GetValues(typeof(TEST));
+            System.Enum.GetName(typeof(TEST), 1);
+            Image image = gameObject.GetComponent<Image>();
+            image.SetActive(!imageActive);
+            imageActive = !imageActive;
+        }
+    }
+}
+
