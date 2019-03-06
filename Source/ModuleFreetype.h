fa9712ba492124c9eeaa19fd7daf90c57d48a94b
aleixgab <aleix.gab@gmail.com>
2019-03-06 17:21:28 +0100

    Remove all Freetype and add SDL_ttf

--- Source/ModuleFreetype.h
+++ Source/ModuleFreetype.h
@@ -2,6 +2,14 @@
 #define __MODULE_FREETYPE_H__
 
 #include "Module.h"
+#include <string>
+
+struct FT_Library;
+struct FT_Face;      
+struct FT_Error;
+struct FT_GlyphSlot;
+struct FT_Matrix;
+struct FT_Vector;
 
 class ModuleFreetype : public Module
 {
@@ -10,9 +18,14 @@ public:
 	ModuleFreetype(bool start_enabled = true);
 	~ModuleFreetype();
 
+	bool Init(JSON_Object * jObject);
 	bool Start();
 	update_status Update();
 
+	bool LoadFont(const char * path);
+
+	void draw_bitmap(FT_Bitmap * bitmap, FT_Int x, FT_Int y);
+	void show_image();
 
 private:
 	FT_Library library;   /* handle to library     */
@@ -20,6 +33,9 @@ private:
 	FT_Error error;
 	FT_GlyphSlot slot;
 	FT_Matrix matrix;
+	FT_Vector pen;
+	std::string text;
+	unsigned char image[640][480];
 };
 
 #endif //__MODULE_FREETYPE_H__
