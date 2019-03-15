ac427052f606d32593441d86cfd102a1acc526b6
DatBeQuiet <kayter72@gmail.com>
2019-03-16 00:09:12 +0100

    testing

--- Source/Raycaster.cpp
+++ Source/Raycaster.cpp
@@ -60,7 +60,7 @@ void Raycaster::ScreenPointToRay(int posX, int posY, float& shortestDistance, ma
 		localSpaceSegment.Transform(hits[i]->transform->GetGlobalMatrix().Inverted());
 
 		if (hits[i]->cmp_mesh == nullptr)
-			continue;
+			return;
 
 		const ResourceMesh* resMesh = (const ResourceMesh*)App->res->GetResource(hits[i]->cmp_mesh->res);
 
