74a53b9d70d1fffa8f7d354d88d351871ea9f154
Julià Mauri Costa <julimacowork@gmail.com>
2019-04-06 18:46:34 +0200

    Revert "Fixed when load scene, UI lost draw buffer info"
    This reverts commit 7b316638685e85afb015d17b8eba584b576f9e52.

--- Source/ModuleUI.cpp
+++ Source/ModuleUI.cpp
@@ -128,7 +128,12 @@ void ModuleUI::OnSystemEvent(System_Event event)
 	switch (event.type)
 	{
 	case System_Event_Type::LoadScene:
+	case System_Event_Type::Stop:
 	{
+		canvas.clear();
+		canvas_screen.clear();
+		canvas_worldScreen.clear();
+		canvas_world.clear();
 		countImages = 0;
 		countLabels = 0;
 		offsetImage = 0;
@@ -139,13 +144,6 @@ void ModuleUI::OnSystemEvent(System_Event event)
 		std::swap(queueImageToBuffer, emptyI);
 		std::queue<ComponentLabel*> emptyL;
 		std::swap(queueLabelToBuffer, emptyL);
-	}
-	case System_Event_Type::Stop:
-	{
-		canvas.clear();
-		canvas_screen.clear();
-		canvas_worldScreen.clear();
-		canvas_world.clear();
 		break;
 	}
 	case System_Event_Type::ComponentDestroyed:
@@ -467,8 +465,6 @@ void ModuleUI::UnRegisterBufferIndex(uint offset, ComponentTypes cType)
 	bool sameOffset = false;
 	if (cType == ComponentTypes::ImageComponent)
 	{
-		if(countImages  != 0)
-		{
 		countImages--;
 		if (sameOffset = (offset == offsetImage - UI_BYTES_IMAGE))
 			offsetImage = offset;
@@ -492,35 +488,31 @@ void ModuleUI::UnRegisterBufferIndex(uint offset, ComponentTypes cType)
 			img->SetBufferRangeAndFIll(offsetSend, offsetSend / sizeof(float) * 4);
 			countImages++;
 		}
-		}
 	}
 	else if (cType == ComponentTypes::LabelComponent)
 	{
-		if (countLabels != 0)
+		countLabels--;
+		if (sameOffset = (offset == offsetLabel + UI_BYTES_LABEL))
+			offsetLabel = offset;
+		else
+			free_label_offsets.push_back(offset);
+		if (!queueLabelToBuffer.empty())
 		{
-			countLabels--;
-			if (sameOffset = (offset == offsetLabel + UI_BYTES_LABEL))
-				offsetLabel = offset;
-			else
-				free_label_offsets.push_back(offset);
-			if (!queueLabelToBuffer.empty())
+			ComponentLabel* lb = queueLabelToBuffer.front();
+			queueLabelToBuffer.pop();
+			uint offsetSend;
+			if (!free_label_offsets.empty())
 			{
-				ComponentLabel* lb = queueLabelToBuffer.front();
-				queueLabelToBuffer.pop();
-				uint offsetSend;
-				if (!free_label_offsets.empty())
-				{
-					offsetSend = free_label_offsets.at(free_label_offsets.size() - 1);
-					free_label_offsets.pop_back();
-				}
-				else
-				{
-					offsetSend = offsetLabel;
-					offsetLabel -= UI_BYTES_LABEL;
-				}
-				lb->SetBufferRangeAndFIll(offsetSend, offsetSend / sizeof(float) * 4);
-				countLabels++;
+				offsetSend = free_label_offsets.at(free_label_offsets.size() - 1);
+				free_label_offsets.pop_back();
 			}
+			else
+			{
+				offsetSend = offsetLabel;
+				offsetLabel -= UI_BYTES_LABEL;
+			}
+			lb->SetBufferRangeAndFIll(offsetSend, offsetSend / sizeof(float) * 4);
+			countLabels++;
 		}
 	}
 }
