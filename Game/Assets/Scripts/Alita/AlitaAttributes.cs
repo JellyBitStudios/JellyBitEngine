e044b25f7b37cded09164c5fcdb26c47321a9277
DatBeQuiet <kayter72@gmail.com>
2019-04-18 14:53:31 +0200

    fix set get currentexp

--- Game/Assets/Scripts/Alita/AlitaAttributes.cs
+++ Game/Assets/Scripts/Alita/AlitaAttributes.cs
@@ -5,21 +5,22 @@ public class AlitaCharacter : Character
 {
     public uint lvl = 1;
     public const float expPerLvlModifier = 20.0f;
+    private float _currentExp;
     public float currentExp
     {
         get //get method for returning value
         {
-            return currentExp;
+            return _currentExp;
         }
         set
         {
             if (value >= lvl * expPerLvlModifier)
             {
                 lvl += 1;
-                currentExp = 0;
+                _currentExp = 0;
             }
             else
-                currentExp = value;
+                _currentExp = value;
         }
     }
 
