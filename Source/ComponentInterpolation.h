32689f9dd88d443210bd5d44e2d5866b2f2826d1
Óscar Hernández <oscarhernandezgomez@gmail.com>
2019-04-24 20:08:35 +0200

    Transnode and save load

--- Source/ComponentInterpolation.h
+++ Source/ComponentInterpolation.h
@@ -12,6 +12,37 @@
 
 struct TransNode
 {
+	TransNode() {}
+	TransNode(char*& cursor)
+	{
+		size_t bytes = sizeof(uint);
+		uint nameLenght;
+		memcpy(&nameLenght, cursor, bytes);
+		cursor += bytes;
+		
+		bytes = nameLenght * sizeof(char);
+		name.resize(nameLenght);
+		memcpy((void*)name.c_str(), cursor, bytes);
+		name.resize(nameLenght);
+		cursor += bytes;
+
+		bytes = sizeof(position);
+		memcpy(&position, cursor, bytes);
+		cursor += bytes;
+
+		bytes = sizeof(rotation);
+		memcpy(&rotation, cursor, bytes);
+		cursor += bytes;
+
+		bytes = sizeof(scale);
+		memcpy(&scale, cursor, bytes);
+		cursor += bytes;
+
+		bytes = sizeof(distance);
+		memcpy(&distance, cursor, bytes);
+		cursor += bytes;
+	}
+
 	std::string name = "No name";
 
 	math::float3 position = math::float3::zero;
@@ -19,6 +50,39 @@ struct TransNode
 	math::float3 scale = math::float3::one;
 
 	float distance = 0;
+
+	uint Size()
+	{
+		return name.size() * sizeof(char) + sizeof(uint) + sizeof(position) + sizeof(rotation) + sizeof(scale) + sizeof(distance);
+	}
+
+	void Save(char* &cursor)
+	{
+		size_t bytes = sizeof(uint);
+		uint nameLenght = name.size();
+		memcpy(cursor, &nameLenght, bytes);
+		cursor += bytes;
+		
+		bytes = nameLenght * sizeof(char);
+		memcpy(cursor, name.c_str(), bytes);
+		cursor += bytes;
+
+		bytes = sizeof(position);
+		memcpy(cursor, &position, bytes);
+		cursor += bytes;
+
+		bytes = sizeof(rotation);
+		memcpy(cursor, &rotation, bytes);
+		cursor += bytes;
+
+		bytes = sizeof(scale);
+		memcpy(cursor, &scale, bytes);
+		cursor += bytes;
+
+		bytes = sizeof(distance);
+		memcpy(cursor, &distance, bytes);
+		cursor += bytes;
+	}
 };
 
 class ComponentInterpolation : public Component
@@ -36,6 +100,12 @@ public:
 	void StartInterpolation(char * nodeName, bool goBack, float time = 0.0f);
 	void GoBack();
 
+	uint GetNodesBytes();
+
+	void SaveNodes(char *& cursor);
+
+	void LoadNodes(char *& cursor, int size);
+
 	//Serialization
 	uint GetInternalSerializationBytes();
 	virtual void OnInternalSave(char*& cursor);
@@ -45,11 +115,12 @@ public:
 
 	std::list<TransNode*> nodes;
 
-	float speed = 1.0f;
+	float speed = 10.0f;
+	float minDist = 1.0f;
 private:
 
 	bool move = false;
-	TransNode* currentNode = nullptr;
+	TransNode currentNode;
 	TransNode startPoint;
 	float currTime = 0;
 
@@ -59,6 +130,7 @@ private:
 	Timer goBackTime;
 
 	bool finished = false;
+	bool goingBack = false;
 };
 
 #endif //! __COMPONENT_INTERPOLATION_H__
